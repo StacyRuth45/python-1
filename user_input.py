69e204bee211cacaec69960b1833870dc4667a57
Stacy Wangui Mwangi <160638172+StacyRuth45@users.noreply.github.com>
2024-02-27 18:38:19 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter Your Name")
+age = int(input("Enter Your Age"))
+location = input("Enter Your Location")
+print("Hello",name,"You are",age,"years old","Your Location is",location)
